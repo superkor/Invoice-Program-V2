b0c107293e568d70d6de04f646d59d991b1e0f88
superkor <86580151+superkor@users.noreply.github.com>
2022-11-11 19:55:28 -0500

    Added 400 handling

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'dd1cfcfaef07bc9acd8d4d6923e9624caafae611169423ff'
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', result = [])
 
 @app.route("/create", methods=["GET"])
 def createInvoice():
@@ -19,10 +19,23 @@ def createInvoice():
     listSessionsDate = request.args.getlist("date-of-session")
 
     numberSessions = len(listSessions)
+    numberAmount = len(listSessionsAmount)
+    numberDate = len(listSessionsDate)
+
+    #raise 400 if session data array doesn't have the same length
+    if (numberAmount != numberSessions or numberSessions != numberDate):
+        abort(400, description="Sessions has missing data "+ str({"session-type": listSessions, "sessions-amount": listSessionsAmount, "date-of-session": listSessionsDate}))
 
     sessions = {}
     #creating dict of sessions
     for x in range (numberSessions):
         sessions["session"+str(x)] = {"type": listSessions[x], "amount": listSessionsAmount[x], "date": listSessionsDate[x]}
 
+    if comments == None:
+        comments = ""
+
     return render_template('index.html', result=[season,month,name,rate, comments, sessions])
+
+@app.errorhandler(400)
+def serverError(e):
+    return render_template("400.html", error = e), 400
